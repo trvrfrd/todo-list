628bd0e6d102c49831f8fab21598d3fade815497
Trevor Ford <info@trvrfrd.net>
2017-01-28 14:40:00 -0500

    add create + delete todos (DOM only)

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
-var addTodoForm = document.forms[0];
+// globals are bad probz
+var form = document.forms[0],
+    textInput = form.querySelector('input[type="text"]'),
+    todos = document.querySelector('#todos');
 
-addTodoForm.addEventListener('submit', function(e) {
-  return false;
+function createElement(name, attrs, text) {
+  var el = document.createElement(name);
+
+  for (var attr in attrs) {
+    el[attr] = attrs[attr];
+  }
+
+  if (text != null) {
+    el.appendChild(document.createTextNode(text));
+  }
+
+  return el;
+}
+
+function todoLi(text) {
+  var li = createElement('li', { class: 'todo' }, text);
+  li.prepend(createElement('input', { type: 'checkbox' }));
+  li.appendChild(createElement('a', { href: '#' }, 'x'));
+  return li;
+}
+
+form.addEventListener('submit', function createTodo(e) {
+  e.preventDefault();
+  var text = textInput.value;
+  if (!text) return false;
+  todos.appendChild(todoLi(text));
+  textInput.value = '';
+});
+
+todos.addEventListener('click', function deleteTodo(e) {
+  if (e.target.nodeName == 'A') {
+    var node = e.target;
+    while (node.nodeName != 'LI') node = node.parentElement;
+    todos.removeChild(node);
+  }
 });
